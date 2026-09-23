0f66eb1fe2cec736b47aeb27fd770710cc3c83c0
Fábio Martins <fabio.martins.work@gmail.com>
2026-09-23 23:43:11 +0100

    Fibonizer: Created functions for recursive and loop without fetching parameters

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -16,7 +19,21 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
+		return c.HTML(http.StatusOK, "WELCOME TO FIBONIZER.\n'/recursive' -> Recursive Fibonizer\n'/loop' -> Loop Fibonizer")
+	})
+
+	e.GET("/recursive", func(c echo.Context) error {
+		start := time.Now()
+		fibo := FibonizeRecursive(8)
+		log.Printf("Time the Recursive Fibonizer took: %d", time.Since(start))
+		return c.HTML(http.StatusOK, strconv.Itoa(fibo))
+	})
+
+	e.GET("/loop", func(c echo.Context) error {
+		start := time.Now()
+		fibo := FibonizeLoop(8)
+		log.Printf("Time the Loop Fibonizer took: %d", time.Since(start))
+		return c.HTML(http.StatusOK, strconv.Itoa(fibo))
 	})
 
 	e.GET("/health", func(c echo.Context) error {
@@ -31,11 +48,35 @@ func main() {
 	e.Logger.Fatal(e.Start(":" + httpPort))
 }
 
-// Simple implementation of an integer minimum
-// Adapted from: https://gobyexample.com/testing-and-benchmarking
-func IntMin(a, b int) int {
-	if a < b {
-		return a
+func FibonizeRecursive(num int) int {
+	if num == 0 {
+		return 0
 	}
-	return b
+
+	if num == 1 {
+		return 1
+	}
+
+	return FibonizeRecursive(num-2) + FibonizeRecursive(num-1)
+}
+
+func FibonizeLoop(num int) int {
+	if num == 0 {
+		return 0
+	}
+
+	if num == 1 {
+		return 1
+	}
+
+	sum := 1
+	previous := 0
+
+	for i := 2; i <= num; i++ {
+		previousSum := sum
+		sum = sum + previous
+		previous = previousSum
+	}
+
+	return sum
 }
